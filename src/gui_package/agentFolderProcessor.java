6f8fcc3725905c3de3a09712f1a0f45c3a3620c1
Jonathan Bair <105879403+jonbair7@users.noreply.github.com>
2024-05-01 18:04:32 -0400

    Update agentFolderProcessor.java

--- src/gui_package/agentFolderProcessor.java
+++ src/gui_package/agentFolderProcessor.java
@@ -14,14 +14,12 @@ import java.io.IOException;
 public class agentFolderProcessor {
      private String outputFile;
 
-     // Constructor
     public agentFolderProcessor(String outputFile) {
         this.outputFile = outputFile;
     }
 
     public void export(String folderName, String creationTime, String lastAccessTime) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile, true))) {
-            // Write folder info to file
             writer.write("Folder: " + folderName + ", Creation Time: " + creationTime + ", Last Access Time: " + lastAccessTime + "\n");
         } catch (IOException e) {
             e.printStackTrace();
